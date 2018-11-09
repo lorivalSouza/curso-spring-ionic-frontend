f84a06e032c36b57688511d6186e1023b2acaf6b
Lorival Souza <lorivalnew@gmail.com>
2018-11-09 17:54:47 -0200

    Tela de escolha do pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -54,7 +54,7 @@ export class PickAddressPage {
 
   nextPage(item: EnderecoDTO) {
     this.pedido.endercoDeEntrega = {id: item.id};
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido});
   }
 
 }
