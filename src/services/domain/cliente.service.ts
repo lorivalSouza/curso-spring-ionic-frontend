12a4410e16ead5da0f06770ad559da690437ae45
Lorival Souza <lorival.per.souza@dxc.com>
2019-02-17 18:55:41 -0300

    Fazendo upload da foto

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,31 +4,52 @@ import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
- @Injectable()
+import { ImageUtilService } from "../image-util.service";
+
+@Injectable()
 export class ClienteService {
-     constructor(public http: HttpClient, public storage: StorageService) {
-    }
-     findByEmail(email: string) {
-         return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService) {
     }
 
     findById(id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
-   }
+    }
+    
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    }
 
-    getImageFromBucket(id: string) : Observable<any> {
+    getImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
         return this.http.get(url, {responseType : 'blob'});
     }
 
-    insert (obj : ClienteDTO) {
+    insert(obj : ClienteDTO) {
         return this.http.post(
-            `${API_CONFIG.baseUrl}/clientes`,
+            `${API_CONFIG.baseUrl}/clientes`, 
             obj,
-            {
-                observe: 'response',
+            { 
+                observe: 'response', 
                 responseType: 'text'
             }
-        );
+        ); 
     }
-} 
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`, 
+            formData,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
+    }
+}
